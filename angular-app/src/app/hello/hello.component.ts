0c91f66fd0e14bfb0d9af6c94dbb9ed507034a4d
hashimoto20540 <hashikazu20540@gmail.com>
2024-08-13 01:43:25 +0900

    learned

---
+++ angular-app/src/app/hello/hello.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css']
+})
+export class HelloComponent implements OnInit {
+  title:string;
+  message:string;
+  styleClass:string;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.title = 'Hello-app';
+    this.message = 'This is My First Component!!';
+    this.styleClass = 'red';
+  }
+
+}
