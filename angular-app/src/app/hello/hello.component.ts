5fa4719d52c5f9594e7360ba1784bdf6c441020a
hashimoto20540 <hashikazu20540@gmail.com>
2024-08-16 17:06:17 +0900

    2章まで終了

--- angular-app/src/app/hello/hello.component.ts
+++ angular-app/src/app/hello/hello.component.ts
@@ -2,20 +2,30 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  templateUrl: './hello.component.html',
-  styleUrls: ['./hello.component.css']
+  styleUrls: ['./hello.component.css'],
+  templateUrl: './hello.component.html'
 })
 export class HelloComponent implements OnInit {
   title:string;
   message:string;
-  styleClass:string;
-
+  nowStyle:any;
+  
   constructor() {}
 
   ngOnInit() {
     this.title = 'Hello-app';
-    this.message = 'This is My First Component!!';
-    this.styleClass = 'red';
+    this.message = 'false,false,false';
+    this.nowStyle = {
+      'border-style':'',
+      'border-width':'',
+      'border-color':''
+    };
   }
 
+  check(in1, in2, in3) {
+    this.nowStyle['border-style'] = in1;
+    this.nowStyle['border-width'] = in2 + "px";
+    this.nowStyle['border-color'] = in3;
+    this.message = JSON.stringify(this.nowStyle);
+  }
 }
