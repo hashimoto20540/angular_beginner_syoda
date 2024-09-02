e3e3bd60c20af784a50b143c4eb1bf680e927816
hashimoto20540 <hashikazu20540@gmail.com>
2024-09-03 08:32:33 +0900

    6章途中

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -22,11 +22,28 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 import {MatInputModule} from '@angular/material/input';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FireComponent } from './fire/fire.component';
+
+var config = {
+  apiKey: "AIzaSyCyImupnO3FOKaLuRrqJ90qPdg_IEDc35Q",
+  authDomain: "book-review-app-1.firebaseapp.com",
+  projectId: "book-review-app-1",
+  storageBucket: "book-review-app-1.appspot.com",
+  messagingSenderId: "67053868730",
+  appId: "1:67053868730:web:e9f6582adc28a3548d5483",
+  measurementId: "G-NPNB6N3K5V"
+}
+
 const routes:Routes = [
+  { path: 'fire', component: FireComponent },
   { path: 'material', component: MaterialComponent },
   { path: 'hello', component: HelloComponent },
   { path: 'msg', component: MessageComponent },
 ];
+
 
 @NgModule({
   declarations: [
@@ -34,7 +51,8 @@ const routes:Routes = [
     HelloComponent,
     MessageComponent,
     MystyleDirective,
-    MaterialComponent
+    MaterialComponent,
+    FireComponent
   ],
   imports: [
     BrowserModule,
@@ -46,7 +64,10 @@ const routes:Routes = [
     RouterModule.forRoot(routes),
     BrowserAnimationsModule,
     MatButtonToggleModule,
-    MatInputModule
+    MatInputModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   bootstrap: [AppComponent],
 })
